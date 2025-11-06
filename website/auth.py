ac63b3fa97046e59734546ea46e7567dd5c57eac
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-11-06 16:42:50 +0100

    templates klaar. 42:00

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login') # Define the login route
 def login():
-    return "<h1>Login Page</h1>"
+    return render_template("login.html")
 
 @auth.route('/logout')# Define the logout route
 def logout():
@@ -12,4 +12,4 @@ def logout():
 
 @auth.route('/sign-up') # Define the sign-up route
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
