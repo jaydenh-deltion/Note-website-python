d743681c396b9fa5d780f104aa0ab64ebc1f39ba
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-11-11 10:44:31 +0100

    ...

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login') # Define the login route
+@auth.route('/login', methods=['GET', 'POST']) # Define the login route
 def login():
     return render_template("login.html", boolean=True)
 
@@ -11,6 +11,24 @@ def login():
 def logout():
     return "<p>Logout Page</p>"
 
-@auth.route('/sign-up') # Define the sign-up route
+@auth.route('/sign-up', methods=['GET', 'POST']) # Define the sign-up route
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to database
+            pass
+
     return render_template("sign_up.html")
